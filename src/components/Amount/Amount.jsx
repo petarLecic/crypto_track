68b2c66b4d4859ac3743ba95924e6a1cdd9753be
petarlecic <petar.lecic94@gmail.com>
2021-05-20 18:42:25 +0200

    event listener, form inside td

--- src/components/Amount/Amount.jsx
+++ src/components/Amount/Amount.jsx
@@ -3,13 +3,25 @@ import { useState } from "react"
 
 const Amount = ({ name, price }) => {
     const [amount, setAmount] = useState(localStorage.getItem(name) ? localStorage.getItem(name) : 0)
-    const [inputValue, setInputValue] = useState('')
+    const [inputValue, setInputValue] = useState(+localStorage.getItem(name) ? localStorage.getItem(name) : '')
+
+    const regex = /^[+]?([0-9]+(?:[.][0-9]*)?|\.[0-9]+)(?:[eE][+-]?[0-9]+)?$/
+
+    function submitAmount(e) {
+        e.preventDefault()
+        if (regex.test(inputValue)) {
+            localStorage.setItem(name, inputValue)
+            setAmount(inputValue)
+        }
+    }
 
     return (
         <>
             <td>
-                <input type='number' value={inputValue ? inputValue : ''} onChange={e => setInputValue(e.target.value)}/>
-                <button>Submit</button>
+                <form onSubmit={e => submitAmount(e)}>
+                    <input type='text' value={inputValue} onChange={e => setInputValue(e.target.value)}/>
+                    <input type='submit' disabled={inputValue === ''}/>
+                </form>
             </td>
             <td>$ {(amount * price).toFixed(2)}</td>
         </>
