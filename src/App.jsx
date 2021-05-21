bbae6e6a1a4e74aaba5f8339aea47c1cd719b67e
petarlecic <petar.lecic94@gmail.com>
2021-05-22 01:10:14 +0200

    show difference since last time

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Single from './components/Single/Single';
 import Table from './components/Table/Table';
 import { StyledApp } from './StyledApp'
 
 function App() {
+    const [ownedCurrencies, setOwnedCurrencies] = useState([])
+    const [ownedValue, setOwnedValue] = useState(0)
+
+    useEffect(() => {
+        let currencies = []
+        let sum = 0
+
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i)
+            let obj = JSON.parse(localStorage.getItem(key))
+            currencies.push({name: key, amount: obj.amount})
+            sum += obj.amount * obj.price
+        }
+        setOwnedCurrencies(currencies)
+        setOwnedValue(sum)
+    }, [])
+
     return (
         <StyledApp>
             <h1 className="main-heading">Crypto Track</h1>
             <Router>
                 <Switch>
                     <Route exact path="/">
-                        <Table/>
+                        <Table ownedCurrencies={ownedCurrencies} ownedValue={ownedValue}/>
                     </Route>
                     <Route path="/currency/:symbol">
                         <Single/>
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,17 +5,30 @@ import { StyledTable } from './StyledTable'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Table = () => {
+const Table = ({ ownedCurrencies, ownedValue}) => {
     const [allCurrencies, setAllCurrencies] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
     const [perPage] = useState(10)
+    const [currentValue, setCurrentValue] = useState(0)
 
     useEffect(() => {
         getCurrencies()
-        // const apiInterval = setInterval(() => getCurrencies(), 60000)
+        const apiInterval = setInterval(() => getCurrencies(), 60000)
 
-        // return () => clearInterval(apiInterval)
+        return () => clearInterval(apiInterval)
     }, [])
+
+    useEffect(() => {
+        if (allCurrencies.length > 0) {
+            let currentValue = 0
+            ownedCurrencies.forEach(curr => {
+                let tmp = allCurrencies.find(el => el.name === curr.name)
+                let tmpValue = tmp?.quote.USD.price * curr.amount
+                if (!isNaN(tmpValue)) currentValue += tmpValue
+            })
+            setCurrentValue(currentValue)
+        }
+    }, [allCurrencies, ownedCurrencies])
 
     function getCurrencies() {
         axios.get('http://localhost:3006').then(res => {
@@ -28,8 +41,11 @@ const Table = () => {
     const firstCurrency = lastCurrency - perPage
     const currencies = allCurrencies.slice(firstCurrency, lastCurrency)
 
+    const difference = (ownedValue - currentValue).toFixed(2)
+
     return allCurrencies.length > 0 ? 
         <>
+            <p className="difference" style={{textAlign: 'center'}}>Since last time: $ {difference}</p>
             <StyledTable>
                 <thead>
                     <TableRow isHeader/>
