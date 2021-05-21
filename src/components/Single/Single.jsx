5044d8aee20d2e93e4c31a21e1316bbde97e56b5
petarlecic <petar.lecic94@gmail.com>
2021-05-21 10:49:16 +0200

    loader for single currency page added

--- src/components/Single/Single.jsx
+++ src/components/Single/Single.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import Loading from "../Loading/Loading"
 
 const Single = () => {
     let symbol = useParams()
@@ -12,7 +13,7 @@ const Single = () => {
                 setCurrency(res.data[symbol.symbol])
                 console.log(res.data[symbol.symbol])
             })
-    }, [])
+    }, [symbol])
 
     return currency ?
         <div>
@@ -21,7 +22,7 @@ const Single = () => {
             <p>{currency.description}</p>
         </div>
         :
-        null
+        <Loading/>
 }
 
 export default Single
